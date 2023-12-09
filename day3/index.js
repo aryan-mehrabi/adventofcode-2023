12dfa8f237a2ac9e47b68c1e2e26a1a0396c2077
aryan <aryan.me77@yahoo.com>
2023-12-09 21:41:28 +0330

    feat: day3 part 2

--- day3/index.js
+++ day3/index.js
@@ -55,6 +55,82 @@ for (var i = 0; i < dataArr.length; i++) {
         }
     }
 }
-console.log(partNums);
-console.log(partNums.reduce(function (acc, curr) { return acc + +curr; }, 0));
-//asdddddd
+// console.log(partNums);
+// console.log(partNums.reduce((acc, curr) => acc + +curr, 0));
+/* ---- PART TWO ---- */
+var getNum = function (i, j) {
+    var num = dataArr[i][j];
+    var y = j + 1;
+    while (isDigit(dataArr[i][y])) {
+        num = num + dataArr[i][y];
+        y++;
+    }
+    y = j - 1;
+    while (isDigit(dataArr[i][y])) {
+        num = dataArr[i][y] + num;
+        y--;
+    }
+    return +num;
+};
+var gears = function (i, j) {
+    var grid = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ];
+    var partNums = [];
+    var checked = Array(9).fill(false);
+    checked[4] = null;
+    grid.forEach(function (_a, index) {
+        var x = _a[0], y = _a[1];
+        if (dataArr[i + x] !== undefined && isDigit(dataArr[i + x][j + y])) {
+            if (index >= 4) {
+                checked[index + 1] = true;
+            }
+            else {
+                checked[index] = true;
+            }
+        }
+    });
+    var num = 0;
+    checked.forEach(function (isNum, index) {
+        if (index === 3 || index === 5) {
+            num = 0;
+        }
+        if (!isNum) {
+            num = 0;
+            return;
+        }
+        if (!num) {
+            var _a = grid[index - (index > 4 ? 1 : 0)], x = _a[0], y = _a[1];
+            num = getNum(i + x, j + y);
+            partNums.push(num);
+        }
+    });
+    return partNums;
+};
+var sumGearRatios = function () {
+    var sum = 0;
+    for (var i = 0; i < dataArr.length; i++) {
+        var row = dataArr[i];
+        for (var j = 0; j < row.length; j++) {
+            var char = row[j];
+            if (char === '*') {
+                var gear = gears(i, j);
+                if (gear.length === 2) {
+                    console.log(gear);
+                    sum = sum + gear[0] * gear[1];
+                }
+            }
+        }
+    }
+    return sum;
+};
+// dhiddd
+var res = sumGearRatios();
+console.log(res);
--- day3/index.ts
+++ day3/index.ts
@@ -56,6 +56,84 @@ for (let i = 0; i < dataArr.length; i++) {
     }
   }
 }
-console.log(partNums);
-console.log(partNums.reduce((acc, curr) => acc + +curr, 0));
-//asdddddd
+// console.log(partNums);
+// console.log(partNums.reduce((acc, curr) => acc + +curr, 0));
+
+/* ---- PART TWO ---- */
+
+const getNum = (i: number, j: number): number => {
+  let num = dataArr[i][j];
+  let y = j + 1;
+  while (isDigit(dataArr[i][y])) {
+    num = num + dataArr[i][y];
+    y++;
+  }
+  y = j - 1;
+  while (isDigit(dataArr[i][y])) {
+    num = dataArr[i][y] + num;
+    y--;
+  }
+  return +num;
+};
+
+const gears = (i: number, j: number) => {
+  const grid = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+  const partNums = [];
+  const checked = Array(9).fill(false);
+  checked[4] = null;
+  grid.forEach(([x, y], index) => {
+    if (dataArr[i + x] !== undefined && isDigit(dataArr[i + x][j + y])) {
+      if (index >= 4) {
+        checked[index + 1] = true;
+      } else {
+        checked[index] = true;
+      }
+    }
+  });
+  let num = 0;
+  checked.forEach((isNum, index) => {
+    if (index === 3 || index === 5) {
+      num = 0;
+    }
+    if (!isNum) {
+      num = 0;
+      return;
+    }
+    if (!num) {
+      const [x, y] = grid[index - (index > 4 ? 1 : 0)];
+      num = getNum(i + x, j + y);
+      partNums.push(num);
+    }
+  });
+  return partNums;
+};
+
+const sumGearRatios = () => {
+  let sum = 0;
+  for (let i = 0; i < dataArr.length; i++) {
+    const row = dataArr[i];
+    for (let j = 0; j < row.length; j++) {
+      const char = row[j];
+      if (char === '*') {
+        const gear = gears(i, j);
+        if (gear.length === 2) {
+          console.log(gear);
+          sum = sum + gear[0] * gear[1];
+        }
+      }
+    }
+  }
+  return sum;
+};
+// dhiddd
+const res = sumGearRatios();
+console.log(res);
